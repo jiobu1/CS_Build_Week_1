697102cb684bd6dc6098f7fd05f2d8e3723f1ca9
Jisha Obukwelu <jisha@obukwelu.com>
2020-10-15 20:15:16 -0400

    outline for project

---
+++ KNN/knn.py
@@ -0,0 +1,27 @@
+import math
+import numpy
+import scipy
+
+# class DIST:
+#     def __init__(self, ):
+#         pass
+
+class KNN:
+    def __init__(self, k):
+        self.k = k
+
+    def fit(self, X_train, y_train):
+        pass
+
+    def predict(self, X_test):
+        pass
+
+
+# create train_test split
+# create accuracy function
+# create different distance choices
+
+
+# https://www.youtube.com/watch?v=ngLyX54e1LU
+# https://github.com/python-engineer/MLfromscratch/blob/master/mlfromscratch/knn.py
+# https://www.tutorialspoint.com/machine_learning_with_python/machine_learning_with_python_knn_algorithm_finding_nearest_neighbors.htm
